37f3cc4448cf97a15129f7da1e2146e4f46f32db
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2021-03-12 16:01:30 +0200

    add Strava athlete type

--- types.ts
+++ types.ts
@@ -66,5 +66,34 @@ export interface IStravaAccessToken {
 }
 
 export interface IStravaAthlete {
-  
+  id: number
+  username: string
+  resource_state: number
+  firstname: string
+  lastname: string
+  bio?: string
+  city?: string
+  state?: string
+  country?: string
+  sex: string
+  premium: boolean
+  summit: boolean,
+  created_at: string
+  updated_at: string
+  badge_type_id: number
+  weight: number
+  profile_medium: string
+  profile: string
+  friend?: number
+  follower?: number
+  follower_count: number
+  friend_count: number
+  mutual_friend_count?: number
+  athlete_type: number
+  date_preference: string
+  measurement_preference: string
+  clubs: any[]
+  ftp: number
+  bikes: any[]
+  shoes: any[]
 }
